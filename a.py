a5792b99d7929106496cc41112d8cba4a980af97
fancyneedmath <543649262@qq.com>
2025-06-06 17:28:37 +0800

    bob-feature的a.py文件修改

--- a.py
+++ a.py
@@ -2,6 +2,8 @@ import pandas as pd
 import requests
 import transformers
 import numpy as pd
-
 a = pd.dataframe()
 # 什么玩意
+
+data_file = pd.read_excel('./test.xlsx')
+data_file.fillna('wu', inplace=True)
